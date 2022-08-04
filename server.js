fb27edaa64e7ed7fe3aeac96e4484eea86f5def4
Sribarathvajasarma <gsribarathvajasarma@gmail.com>
2022-08-04 19:34:28 +0530

    socket test3

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
 io.on('connection', socket => {
-    console.log('a user connected');
+    console.log(socket.id + 'Connected');
 
 })
 
