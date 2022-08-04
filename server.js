bf22ee77b3ede8045dded1ef7cbe19b5496073c4
Sribarathvajasarma <gsribarathvajasarma@gmail.com>
2022-08-04 19:01:27 +0530

    socket test2

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 
-const http = require('http').createServer(app)
+const https = require('https').createServer(app)
 const io = require('socket.io')(http)
 
 io.on('connection', socket => {
@@ -30,6 +30,6 @@ app.use('/api', require('./routes/listingRouter'))
 
 
 const port = process.env.PORT || 5000
-http.listen(port, () => {
+https.listen(port, () => {
     console.log('Server is running on port', port)
 })
