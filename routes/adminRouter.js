ba8d0e37513d128ed6fbd438b9d2ba0f3ccb9fbb
Thishan-Paramanayagam <84135246+Thishan-Paramanayagam@users.noreply.github.com>
2022-10-16 17:39:43 +0530

    adrouter

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const listingCtrl = require("../controllers/listingCtrl")
 const userCtrl = require("../controllers/userCtrl")
+const compCtrl = require("../controllers/compCtrl")
 
 router.get('/anlistings', listingCtrl.getListings)
 router.get('/users', userCtrl.getUsers)
-
+router.get('/complaints', compCtrl.getComplaints)
 
 
 
