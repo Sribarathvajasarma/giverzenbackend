e0ace648992f55a521531cb14ec1efc5d478ba3d
Thishan-Paramanayagam <84135246+Thishan-Paramanayagam@users.noreply.github.com>
2022-10-16 06:41:08 +0530

    adminrouterup

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
 const listingCtrl = require("../controllers/listingCtrl")
-
+const userCtrl = require("../controllers/userCtrl")
 
 router.get('/anlistings', listingCtrl.getListings)
-
+router.get('/users', userCtrl.getUsers)
 
 
 
