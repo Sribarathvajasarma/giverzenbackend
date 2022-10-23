d2072f3987d0c7cbea89b81c8d38c18f98353e90
Thishan-Paramanayagam <84135246+Thishan-Paramanayagam@users.noreply.github.com>
2022-10-23 17:02:16 +0530

    admrouter

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,11 +2,13 @@ const router = require('express').Router()
 const listingCtrl = require("../controllers/listingCtrl")
 const userCtrl = require("../controllers/userCtrl")
 const compCtrl = require("../controllers/compCtrl")
+const adminauthCtrl = require("../controllers/adminauthCtrl")
+const adDriverCtrl = require("../controllers/adDriverCtrl")
 
 router.get('/anlistings', listingCtrl.getListings)
 router.get('/users', userCtrl.getUsers)
 router.get('/complaints', compCtrl.getComplaints)
-
-
+router.post('/adlogin', adminauthCtrl.adlogin)
+router.get('/drivers', adDriverCtrl.getDrivers)
 
 module.exports = router
