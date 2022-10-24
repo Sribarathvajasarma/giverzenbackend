e1356c1078c1b967b4ae1ebac522140a816f1e0c
Thishan-Paramanayagam <84135246+Thishan-Paramanayagam@users.noreply.github.com>
2022-10-24 12:39:47 +0530

    compctrl

--- controllers/compCtrl.js
+++ controllers/compCtrl.js
@@ -24,6 +24,48 @@ const compCtrl = {
         }
     },
 
+    addComplaints: async (req, res) => {
+        try {
+            db.query("INSERT INTO complaints ( username, complaint, reportedto,date) VALUES ('" + username + "', '" + complaint + "','" + reportedto + "','" + date + "')", (err, results) => {
+                if (err) {
+                    throw err
+                }
+                if (results){
+                    res.json({
+                        results
+                    })
+                }
+
+            })
+
+        } catch (err) {
+            return res.status(500).json({ msg: err.message })
+        }
+    },
+
+    deleteComplaints: async (req, res) => {
+        try {
+            
+
+
+            db.query("DELETE FROM complaints", (err, ressults) => {
+                if (err) {
+                    throw err
+                } else {
+                    res.json({
+                        msg: "Complaints removed successfully!"
+                    })
+                }
+
+            })
+
+
+
+
+        } catch (err) {
+            return res.status(500).json({ msg: err.message })
+        }
+    },
 }
 
 module.exports = compCtrl
