384f26624128e7021804646ebefc59125e683357
Thuvethika <thuvethika@gmail.com>
2022-10-26 14:45:44 +0530

    dashboard ctrl

--- controllers/dashCtrl.js
+++ controllers/dashCtrl.js
@@ -1,6 +1,7 @@
 const db = require('../config/db')
 
 const dashCtrl = {
+    // selecting user count in admin dashboard
     getTotusers: async (req, res) => {
         try {
             db.query("Select count(id) as count1 FROM user", (err, results) => {
@@ -19,8 +20,7 @@ const dashCtrl = {
             return res.status(500).json({ msg: err.message })
         }
     },
-
-
+ //  select listing count in admin dashboard
     getTotlistings: async (req, res) => {
         try {
             db.query("Select count(id) as count3 FROM listings", (err, results) => {
@@ -39,6 +39,8 @@ const dashCtrl = {
             return res.status(500).json({ msg: err.message })
         }
     },
+
+    //select driver count in admin dashboard
     getTotdrivers: async (req, res) => {
         try {
             db.query("Select count(id) as count4 FROM driver", (err, results) => {
@@ -57,7 +59,7 @@ const dashCtrl = {
             return res.status(500).json({ msg: err.message })
         }
     },
-
+// select complaint count in admin dashboard
     getTotcomplaints: async (req, res) => {
         try {
             db.query("Select count(complaint_id) as count2 FROM complaints", (err, results) => {
@@ -77,6 +79,7 @@ const dashCtrl = {
         }
     },
 
+    //select request count in admin dashboard
     getTotwanted: async (req, res) => {
         try {
             db.query("Select count(id) as count5 FROM requests", (err, results) => {
